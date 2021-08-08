63bfe8bf31e2d33e84a14b2a7497fbb91465a3b5
MuhammadSalman009-tech <salmansumra009@gmail.com>
2021-08-08 13:30:38 +0500

    Lec6(Navigation):added native stack navigation

--- App.js
+++ App.js
@@ -1,4 +1,43 @@
-<script src="http://localhost:8097"></script>;
+import * as React from "react";
+import { View, Text } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+
+function HomeScreen() {
+  return (
+    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+      <Text>Home Screen</Text>
+    </View>
+  );
+}
+function DetailsScreen() {
+  return (
+    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+      <Text>Details Screen</Text>
+    </View>
+  );
+}
+const Stack = createNativeStackNavigator();
+
+function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen
+          name="Home"
+          component={HomeScreen}
+          options={{ title: "Overview" }}
+        />
+        <Stack.Screen name="Details" component={DetailsScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+export default App;
+
+{
+  /* <script src="http://localhost:8097"></script>;
 import { StatusBar } from "expo-status-bar";
 import Contacts, { compareNames } from "./Contacts";
 import React from "react";
@@ -71,4 +110,5 @@ const styles = StyleSheet.create({
   count: {
     fontSize: 30,
   },
-});
+}); */
+}
---
+++ navigation-examples/0-Switch.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { Button, View } from "react-native";
+
+function HomeScreen() {
+  return (
+    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+      <Text>Home Screen</Text>
+    </View>
+  );
+}
+const Stack = createNativeStackNavigator();
